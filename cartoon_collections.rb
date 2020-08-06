9dfd71380c114baafc719571c24cb44592c2e2b9
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-06 23:31:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,24 +3,24 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet(planeteer_calls)
-    new_planeteer_calls = planeteer_calls.collect {|call| call.capitalize + "!"}
-end
+# def summon_captain_planet(planeteer_calls)
+#     new_planeteer_calls = planeteer_calls.collect {|call| call.capitalize + "!"}
+# end
  
-def long_planeteer_calls(calls)
-  if calls.length.any? {|call| call.length.any? > 4}
-end
+# def long_planeteer_calls(calls)
+#   if calls.length.any? {|call| call.length.any? > 4}
+# end
 
-def find_the_cheese(foods)
-    cheese_types = ["cheddar", "gouda", "camembert"]
-    keepers = []
-    if foods.find {|food_item| cheese_types.include?(food_item)}
-      return foods.find{|food_item| cheese_types.include?(food_item)}
-    else 
-      return nil
-    end 
-    keepers
-end
+# def find_the_cheese(foods)
+#     cheese_types = ["cheddar", "gouda", "camembert"]
+#     keepers = []
+#     if foods.find {|food_item| cheese_types.include?(food_item)}
+#       return foods.find{|food_item| cheese_types.include?(food_item)}
+#     else 
+#       return nil
+#     end 
+#     keepers
+# end
 
 
 # def find_the_cheese(food)
