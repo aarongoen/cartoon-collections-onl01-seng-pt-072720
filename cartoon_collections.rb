e2b78b516d255f2fe90d7e092afe7638e1e15039
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-06 23:47:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,11 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf,i| 
-    puts "#{i}. #{dwarf}"
+  dwarves.each_with_index {|dwarf,i| puts "#{i}. #{dwarf}"}
   end
+  
+  # def roll_call_dwarves(dwarves)
+  # dwarves.each_with_index do |dwarf,i| 
+  #   puts "#{i}. #{dwarf}"
+  # end
 
 # def summon_captain_planet(planeteer_calls)
 #     new_planeteer_calls = planeteer_calls.collect {|call| call.capitalize + "!"}
