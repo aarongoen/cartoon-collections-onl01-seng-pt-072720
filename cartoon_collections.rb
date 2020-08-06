379ef09b5cfb3dbc9b8549014daa87834891dafb
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-06 23:58:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,10 @@
-# def roll_call_dwarves(dwarves)
-#   dwarves.each_with_index {|dwarf,i| puts "#{i+1}. #{dwarf}"}
-# end
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|dwarf,i| puts "#{i+1}. #{dwarf}"}
+end
   
-# def summon_captain_planet(planeteer_calls)
-#     new_planeteer_calls = planeteer_calls.map {|call| call.capitalize + "!"}
-# end
+def summon_captain_planet(planeteer_calls)
+    new_planeteer_calls = planeteer_calls.map {|call| call.capitalize + "!"}
+end
  
 def long_planeteer_calls(calls)
   if calls.length.any? {|call| call.length.any? > 4}
