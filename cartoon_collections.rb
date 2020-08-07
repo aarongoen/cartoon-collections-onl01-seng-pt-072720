b95ba5d906965f585793f80cb113bb5c4288b3fe
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-07 00:08:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
  
 def long_planeteer_calls(calls)
-  calls.each {|call| call.length.any? > 4}
+  new_calls = calls.each {|call| call.length.any? > 4}
 end
 
 def find_the_cheese(foods)
